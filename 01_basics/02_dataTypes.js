98ec45899a7938b3ddb0baede9e5a0ae09294b06
Trupti pawar <pawartrupti2002@gmail.com>
2025-02-10 00:12:37 +0530

    done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";  //treat all js code as newer version
+
+//alert ( 3 + 3 ) //we are using the nodejs, not browser
+
+console.log("Happy")
+
+let name = "happiness"
+let age = 21
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value 
+// undefines => 
+// symbol => unique 
+
+// object
+
+console.log(typeof "happiness");
+
+
+
+
