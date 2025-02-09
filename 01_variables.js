a053edeb1f813b8c591fdf409477920f56a8a415
Trupti pawar <pawartrupti2002@gmail.com>
2025-02-09 16:45:17 +0530

    dene

---
+++ 01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 332211
+let accountEmai = "truptip@gmail.com"
+var accountPassword = "12344"
+accountCity = "Pune"
+let accountState = "Maharashtra";
+// accountId = 2 # This is wrong, you can't reassign or change value of a const variable
+// TypeError: Assignment to constant variable. #  Try to read and understand errors, you will find it their most probably
+
+accountEmai = "hr@hc.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+
+
+console.log(accountId); 
+/*  
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmai, accountPassword, accountCity, accountState])
