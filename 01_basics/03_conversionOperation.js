ae802585bb25e752ec34a5aad8936d671761c3eb
Trupti pawar <pawartrupti2002@gmail.com>
2025-02-16 01:02:56 +0530

    modified

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -49,16 +49,23 @@ let str2 = " Trupti"
 let str3 = str1 + str2
 console.log(str3);
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1"+ 2 + 2);
-console.log( 1 + 2 + "2");
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+ 2 + 2);
+// console.log( 1 + 2 + "2");
+// console.log( (3 + 4) * 5 % 3);
 
 
+// console.log(+true);
+// console.log(+"");
 
+let num1, num2, num3
 
+num1 = num2 = num3 = 2 + 2
 
-
-
+let gameCounter = 100
+++gameCounter;    // prefix increment
+// gameCounter++;    // postfix increment
+console.log(gameCounter);
 
 
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,55 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+// === check both value strict Equality 
+// == loose Equality
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
