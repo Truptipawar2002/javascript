60a6ddcea421a184f8bb0190064a5e38b5143a76
Trupti pawar <pawartrupti2002@gmail.com>
2025-04-11 23:28:51 +0530

    update

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -10,9 +10,34 @@ for (let i = 0; i <= 10; i++) {
     // console.log(element);
 }
 
-for (let i = 0; i <= 10; i++) {
-    for (let index = 0; index < Array.length; index++){
-        const element = Array[index];
+for (let i = 1; i <= 5; i++) {
+    // console.log(`VALUE OF I IS ${i}`);
+    
+    for (let j = 1; j <= 5; j++){
+        // console.log(`VALUE OF J IS ${j} AND I IS ${i}`);
+        // console.log(i + '*' + j + '=' + i * j);
+        
+    
     }
 }
 
+let myArray = ["apple", "banana", "orange"]
+// console.log(myArray.length);
+for (let i = 0; i < myArray.length; i++) {
+    // console.log(myArray[i]);
+    const element = myArray[i];
+    // console.log(`Element is ${element}`);
+    
+}
+
+// break and continue
+
+for (let i = 1; i <= 20; i++) {
+     console.log(`VALUE OF I IS ${i}`);
+    if (i === 5) {
+        console.log(`detected 5`);
+         continue; // skip the rest of the loop and continue with the next iteration
+    }
+    console.log(`VALUE OF I IS ${i}`);
+    
+}
