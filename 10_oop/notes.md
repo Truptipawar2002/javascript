2f633243987a4eaa8b94433de026dea02bd2e92f
Trupti pawar <pawartrupti2002@gmail.com>
2025-04-23 22:11:01 +0530

    oop concepts

---
+++ 10_oop/notes.md
@@ -0,0 +1,21 @@
+# javascript and classes oop
+
+## object
+- collection of properties and method'
+- toLowerCase
+
+## why use oop
+
+## parts of oop
+object literal
+
+- Constructor functions - 
+- Prototypes
+- Classes 
+- Instances(new, this)
+
+## 4 pillars
+Abstraction 
+Encapsulation
+Inheritance
+Polymorphism
---
+++ 10_oop/oop.js
@@ -0,0 +1,32 @@
+const user = {
+    username: "trupti",
+    loginCount: 8,
+    SignedIn: true,
+
+    getUserDetails: function(){
+        // console.log("get user details from database");
+        // console.log(`Username: ${this.username}`);
+       // console.log(this);
+    }
+}
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+function User(username, logincount, isLoggedIn){
+    this.username = username;
+    this.logincount = logincount;
+    this.isLoggedIn = isLoggedIn
+
+    // return this
+
+}
+
+const userOne = new  User("trupti", 13, true)
+const userTwo = new User("sanju", 14, false)
+console.log(userOne);
+console.log(userTwo);
+
+
+
